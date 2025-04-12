561cab733cd565e95a9f8a05dfc80657ff98e8b6
Tamara Innomesanghan <38911322+itamarack@users.noreply.github.com>
2025-04-12 12:44:34 +0100

    Add Category model and migration for managing user task categories

---
+++ app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Category extends Model
+{
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use HasFactory, Notifiable, HasApiTokens;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id'
+    ];
+}
